42fbc945b94ca5903d2ffee544e5d05378aff87a
yotam <ynagor@gmail.com>
2021-09-12 23:17:50 +0300

    [DONE]

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const nocache = require('nocache');
 
 const app = express();
 
 app.use(express.static(__dirname + '/dist/bzaat-epk'));
-app.use(nocache());
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/bzaat-epk/index.html'));
